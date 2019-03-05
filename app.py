408edb80cc4c95856c6666c7bbba01355763c39d
Oshinko <osnk@renjaku.jp>
2019-03-06 08:55:29 +0900

    Add new models

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import config
-import datetime
+from datetime import datetime
 from flask import Flask, jsonify, redirect, render_template, request, url_for
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+from os import urandom
 from pathlib import Path
+
+
+def rand16hex():
+    return urandom(16).hex()
+
 
 conf = config.read(Path.home() / '.kyak')
 
@@ -17,33 +23,138 @@ ma = Marshmallow(app)
 
 class Account(db.Model):
     __tablename__ = 'accounts'
-    id = db.Column(db.String(), primary_key=True)
+    id = db.Column(db.String(16), primary_key=True)
+    type = db.Column(db.String(16), nullable=False, default='personal')
     name = db.Column(db.String(), nullable=False)
-    type = db.Column(db.String(), nullable=False, default='personal')
     email = db.Column(db.String(), nullable=True)
     address = db.Column(db.String(), nullable=True)
-    created = db.Column(db.DateTime, nullable=False,
-                        default=datetime.datetime.utcnow)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated = db.Column(db.DateTime, nullable=True)
 
 
 class Access(db.Model):
     __tablename__ = 'accesses'
-    account_id = db.Column(db.String(), primary_key=True)
-    owner = db.Column(db.String(), primary_key=True)
+    account_id = db.Column(db.String(16), primary_key=True)
+    owner = db.Column(db.String(16), primary_key=True)
     access = db.Column(db.String(), nullable=False)
-    created = db.Column(db.DateTime, nullable=False,
-                        default=datetime.datetime.utcnow)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated = db.Column(db.DateTime, nullable=True)
 
 
 class Hook(db.Model):
     __tablename__ = 'hooks'
-    account_id = db.Column(db.String(), primary_key=True)
-    type = db.Column(db.String(), primary_key=True, nullable=False)
+    account_id = db.Column(db.String(16), primary_key=True)
+    type = db.Column(db.String(16), primary_key=True)
     url = db.Column(db.String(), nullable=False)
-    created = db.Column(db.DateTime, nullable=False,
-                        default=datetime.datetime.utcnow)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated = db.Column(db.DateTime, nullable=True)
+
+
+class Offer(db.Model):
+    __tablename__ = 'offers'
+    account_id = db.Column(db.String(16), primary_key=True)
+    offeror = db.Column(db.String(16), primary_key=True)
+    c = db.Column(db.String(32), primary_key=True, default=rand16hex)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated = db.Column(db.DateTime, nullable=True)
+
+
+class Contract(db.Model):
+    __tablename__ = 'contracts'
+    account_id = db.Column(db.String(16), primary_key=True)
+    contractor = db.Column(db.String(16), primary_key=True)
+    contractee = db.Column(db.String(16), primary_key=True)
+    c = db.Column(db.String(32), primary_key=True, default=rand16hex)
+    type = db.Column(db.String(16), nullable=False)
+    since = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    until = db.Column(db.DateTime, nullable=True)
+    payment_terms = db.Column(db.String(), nullable=True)
+    deposit = db.Column(db.Integer, nullable=True)
+    contractor_signed = db.Column(db.DateTime, nullable=True)
+    contractee_signed = db.Column(db.DateTime, nullable=True)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated = db.Column(db.DateTime, nullable=True)
+
+
+class Term(db.Model):
+    __tablename__ = 'terms'
+    account_id = db.Column(db.String(16), primary_key=True)
+    contractor = db.Column(db.String(16), primary_key=True)
+    contractee = db.Column(db.String(16), primary_key=True)
+    c = db.Column(db.String(32), primary_key=True, default=rand16hex)
+    cc = db.Column(db.String(32), primary_key=True, default=rand16hex)
+    order = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated = db.Column(db.DateTime, nullable=True)
+
+
+class TimeAndMaterialsPrice(db.Model):
+    __tablename__ = 'time_and_materials_prices'
+    account_id = db.Column(db.String(16), primary_key=True)
+    contractor = db.Column(db.String(16), primary_key=True)
+    contractee = db.Column(db.String(16), primary_key=True)
+    c = db.Column(db.String(32), primary_key=True, default=rand16hex)
+    cc = db.Column(db.String(32), primary_key=True, default=rand16hex)
+    until = db.Column(db.Float, nullable=False)
+    price = db.Column(db.Float, nullable=False, default=0)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated = db.Column(db.DateTime, nullable=True)
+
+
+class TimeAndMaterialsActivity(db.Model):
+    __tablename__ = 'time_and_materials_activities'
+    account_id = db.Column(db.String(16), primary_key=True)
+    contractor = db.Column(db.String(16), primary_key=True)
+    contractee = db.Column(db.String(16), primary_key=True)
+    c = db.Column(db.String(32), primary_key=True, default=rand16hex)
+    cc = db.Column(db.String(32), primary_key=True, default=rand16hex)
+    day = db.Column(db.Integer, nullable=False)
+    since = db.Column(db.DateTime, nullable=True)
+    until = db.Column(db.DateTime, nullable=True)
+    description = db.Column(db.String(), nullable=True)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated = db.Column(db.DateTime, nullable=True)
+
+
+class Payment(db.Model):
+    __tablename__ = 'payments'
+    account_id = db.Column(db.String(16), primary_key=True)
+    contractor = db.Column(db.String(16), primary_key=True)
+    contractee = db.Column(db.String(16), primary_key=True)
+    c = db.Column(db.String(32), primary_key=True, default=rand16hex)
+    amount = db.Column(db.Float, nullable=False)
+    tax = db.Column(db.Float, nullable=False)
+    requested = db.Column(db.DateTime, nullable=True)
+    paid = db.Column(db.DateTime, nullable=True)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated = db.Column(db.DateTime, nullable=True)
+
+
+class ContractTemplates(db.Model):
+    __tablename__ = 'contract_templates'
+    account_id = db.Column(db.String(16), primary_key=True)
+    contractor = db.Column(db.String(16), primary_key=True)
+    contractee = db.Column(db.String(16), primary_key=True)
+    c = db.Column(db.String(32), primary_key=True, default=rand16hex)
+    cc = db.Column(db.String(32), primary_key=True, default=rand16hex)
+    title = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated = db.Column(db.DateTime, nullable=True)
+
+
+class TermTemplates(db.Model):
+    __tablename__ = 'term_templates'
+    account_id = db.Column(db.String(16), primary_key=True)
+    contractor = db.Column(db.String(16), primary_key=True)
+    contractee = db.Column(db.String(16), primary_key=True)
+    c = db.Column(db.String(32), primary_key=True, default=rand16hex)
+    cc = db.Column(db.String(32), primary_key=True, default=rand16hex)
+    title = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=False)
+    created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated = db.Column(db.DateTime, nullable=True)
 
 
